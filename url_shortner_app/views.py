f52879d315b2b6909ae6cd6e4852b4eb810d0aea
ValeryKharkov <kharkov.valery@gmail.com>
2023-02-07 14:22:33 +0300

    Проект успешно закончен. Но не запускается в связи с местоположением (Country not allowed)

--- url_shortner_app/views.py
+++ url_shortner_app/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+import uuid
+from .models import Url
+from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+
+def create(requests):
+    if requests.method == 'POST':
+        link = requests.POST['link']
+        uid = str(uuid.uuid4())[:5]
+        new_url = Url(link=link, uuid=uid)
+        new_url.save()
+        return HttpResponse(uid)
+
+def go(requests, pk):
+    url_details = Url.objects.get(uuid=pk)
+    return redirect('http://' + url_details.link)
